13e74ced7edda316257489fb566664e68e6d8309
DatHC <huynhcongdat1999@gmail.com>
2019-10-30 16:25:27 +0700

    first commit

---
+++ MyDiaryApp/app/src/main/java/congdat/hcd/mydiaryapp/DangKyActivity.java
@@ -0,0 +1,14 @@
+package congdat.hcd.mydiaryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DangKyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_dangky);
+    }
+}
